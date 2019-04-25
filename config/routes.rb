7422ba42902b93c9607cf35f4222c366a96485c5
CristiAllan <cristiallan@gmail.com>
2019-04-25 13:19:39 -0300

    add destroy_done endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    collection do
+      delete :destroy_done
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
